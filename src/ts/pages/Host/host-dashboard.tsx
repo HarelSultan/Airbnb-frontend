fe92d4a02769bb83f57b63177f180e1b5eefb257
Harel Sultan <harel714@gmail.com>
2023-05-16 19:24:08 +0300

    Modified HostDashboard: now loading host reservations from the server. Fixed listingsName duplicity. Modified css common styling into variables.

--- src/ts/pages/Host/host-dashboard.tsx
+++ src/ts/pages/Host/host-dashboard.tsx
@@ -1,54 +1,72 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
+import { reservationService } from '../../services/reservation.service'
 import { stayService } from '../../services/stay.service'
-import { changeReservationStatus } from '../../store/user/user.action'
 
+import { UserPageHeader } from '../../cmps/user-page-header'
 import { HostCharts } from './host-charts'
 import { Reservations } from './reservations'
 
-import { ReservationProps } from '../../interfaces/user-interface'
 import { RootStateProps } from '../../store/store'
-import { UserPageHeader } from '../../cmps/user-page-header'
+import { ReservationProps } from '../../interfaces/user-interface'
 
 export function HostDashboard() {
     const loggedInUser = useSelector((storeState: RootStateProps) => storeState.userModule.loggedInUser)
+    const [reservations, setReservations] = useState<ReservationProps[] | null>(null)
     const navigate = useNavigate()
 
     useEffect(() => {
         if (!loggedInUser) navigate('/')
+        loadHostReservations()
     }, [])
 
-    const onChangeReservationStatus = async (reservation: ReservationProps, isApproved: boolean) => {
-        if (!loggedInUser) return
+    const loadHostReservations = async () => {
+        if (!loggedInUser?.listingsId.length) return
         try {
-            await changeReservationStatus(loggedInUser, reservation, isApproved)
+            const hostReservations = await reservationService.getHostReservations(loggedInUser._id)
+            setReservations(hostReservations)
+        } catch (err) {
+            // TODO:  ShowErrorMsg('Cannot load reservations, try again later')
+            console.log('Failed to load host reservations with error:', err)
+        }
+    }
+
+    const onChangeReservationStatus = async (reservation: ReservationProps, isApproved: boolean) => {
+        if (!loggedInUser || !reservations) return
+        try {
+            const updatedStatus = isApproved ? 'approved' : 'rejected'
+            const updatedReservation: ReservationProps = { ...reservation, status: updatedStatus }
+            const updatedReservations: ReservationProps[] = reservations?.map(reservation =>
+                reservation._id === updatedReservation._id ? updatedReservation : reservation
+            )
+            await reservationService.update(updatedReservation)
+            setReservations(updatedReservations)
         } catch (err) {
             // TODO: showErrorMsg(err.txt)
         }
     }
 
-    const listingsName = loggedInUser?.listings?.map(listing => listing.name) || ['']
-
-    if (loggedInUser?.listingReservations) {
-        const data = stayService.getHostDashboardData(loggedInUser.listingReservations)
-    }
+    const listingsName = reservations?.reduce((acc: string[], reservation) => {
+        if (!acc.includes(reservation.stayName)) acc.push(reservation.stayName)
+        return acc
+    }, []) || ['']
 
     return (
         <section className='main-layout host-dashboard'>
             <UserPageHeader loggedInUser={loggedInUser} />
 
-            {!loggedInUser?.listingReservations?.length ? (
+            {!reservations?.length ? (
                 <div>No Reservations</div>
             ) : (
                 <>
                     <HostCharts
                         listingsName={listingsName}
-                        hostChartData={stayService.getHostDashboardData(loggedInUser.listingReservations)}
+                        hostChartData={stayService.getHostDashboardData(reservations)}
                     />
                     <Reservations
-                        listingReservations={loggedInUser.listingReservations}
+                        listingReservations={reservations}
                         onChangeReservationStatus={onChangeReservationStatus}
                     />
                 </>
